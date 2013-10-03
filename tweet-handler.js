20cecddd00a621548ec7c0122e7bfdc0c79161fe
David Kua <david@kua.io>
2013-10-03 17:47:01 -0400

    Added basic JSON responses. For /tweets and /users.

--- tweet-handler.js
+++ tweet-handler.js
@@ -5,11 +5,29 @@ var link = require("./link-handler");
 
 
 var handler = function(req, res, json) {
-  json = JSON.parse(json);
-  res.setHeader("Content-Type", "text/html");
+  urlArray = req.url.toLowerCase().split("/");
+
+  if (urlArray[1] == "tweets") {
+    var tweets = new Array();
+    for (var i=0; i<json.length; i++) {
+      item = [
+        "#" + json[i].id,
+        json[i].text,
+        json[i].user.screen_name + " - " + json[i].created_at.split(" ").slice(0, 4).join(" ")
+      ].join("<br>");
+
+      tweets.push(item);
+    }
+    pageStatus = 200;
+    data = tweets;
+  }
+
+  console.log(data);
+  res.setHeader("Content-Type", "application/json");
   res.writeHead(pageStatus);
-  data = "Tweets";
-  res.end(data);
+  res.write(JSON.stringify(data));
+  res.end();
 }
 
 module.exports.handler = handler;
+
--- user-handler.js
+++ user-handler.js
@@ -4,11 +4,23 @@ var path = require("path");
 
 
 var handler = function(req, res, json) {
-  json = JSON.parse(json);
-  res.setHeader("Content-Type", "text/html");
+  urlArray = req.url.toLowerCase().split("/");
+
+  if (urlArray[1] == "users") {
+    var users = new Array();
+    for (var i=0; i<json.length; i++) {
+      item = json[i].user.name + " (" + json[i].user.screen_name + ")";
+      users.push(item);
+    }
+    pageStatus = 200;
+    data = users;
+  }
+
+  console.log(data);
+  res.setHeader("Content-Type", "application/json");
   res.writeHead(pageStatus);
-  data = "Users";
-  res.end(data);
+  res.write(JSON.stringify(data));
+  res.end();
 }
 
 module.exports.handler = handler;
