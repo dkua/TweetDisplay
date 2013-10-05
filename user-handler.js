4e4bbc1581b5293625d39790153f1e0d5ae7a16d
David Kua <david@kua.io>
2013-10-05 17:28:08 -0400

    Finished User Handler

--- user-handler.js
+++ user-handler.js
@@ -3,19 +3,65 @@ var fs = require("fs");
 var path = require("path");
 
 
-var handler = function(req, res, json) {
-  urlArray = req.url.toLowerCase().split("/");
-
-  if (urlArray[1] == "users") {
-    var users = new Array();
-    for (var i=0; i<json.length; i++) {
-      item = json[i].user.name + " (" + json[i].user.screen_name + ")";
-      users.push(item);
+function listUsers(json) {
+  var data = new Array();
+  for (var i=0; i<json.length; i++) {
+    item = {
+      "id": json[i].user.id,
+      "name": json[i].user.name,
+      "screen_name": json[i].user.screen_name,
     }
-    pageStatus = 200;
-    data = users;
+    data.push(item);
   }
+  return data;
+}
 
+function getUser(id, json) {
+  for (var i=0; i<json.length; i++) {
+    if (json[i].user.id_str === id) {
+      return json[i];
+    }
+  }
+}
+
+function getUserTweets(id, json) {
+  var data = new Array();
+  for (var i=0; i<json.length; i++) {
+    if (json[i].user.id_str === id) {
+      item = {
+        "tweet": json[i].text,
+        "user": json[i].user.screen_name,
+        "date": json[i].created_at.split(" ").slice(0, 4).join(" ")
+      }
+      data.push(item);
+    }
+  }
+  return data;
+}
+
+var handler = function(req, res, json) {
+  var urlArray = req.url.toLowerCase().split("/");
+  var data = new Array();
+  var pageStatus = 404;
+
+  switch (urlArray.length) {
+    case 2:
+      if (urlArray[1] == "users") {
+        pageStatus = 200;
+        data = data.concat(listUsers(json));
+      }
+    case 3:
+      if (!isNaN(urlArray[2])) {
+        pageStatus = 200;
+        item = getUser(urlArray[2], json);
+        data.push(item);
+      }
+    case 4:
+      if (urlArray[3] === "tweets") {
+        pageStatus = 200;
+        data = data.concat(getUserTweets(urlArray[2], json));
+      }
+  }
   console.log(data);
   res.setHeader("Content-Type", "application/json");
   res.writeHead(pageStatus);
@@ -24,3 +70,4 @@ var handler = function(req, res, json) {
 }
 
 module.exports.handler = handler;
+
